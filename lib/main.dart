0b84848b1eff6555933299e9e5a9420f9dfd8a1b
Ali-M-Jradi <lab07@lab.mu.edu>
2025-11-11 12:19:13 +0200

    final submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(home: QuoteApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class QuoteApp extends StatefulWidget {
+  const QuoteApp({super.key});
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  State<QuoteApp> createState() => _QuoteAppState();
 }
+
+class _QuoteAppState extends State<QuoteApp> {
+  String quote = 'Tap "New Quote" to get motivated ✨';
+  String symbol = '❓';
+
+  Enum choice(){
+    doit;
+    keep;
+    belive;
+  }
+  void motivate(){
+
+    setState(() {
+
+      if(choice==doit){
+        quote='you can do it!';
+        symbol='🚀';
+      }
+      else if(choice==keep){
+        quote='keep going!';
+        symbol='💪';
+      }
+      else{
+        quote='believe in your self!';
+        symbol='🌟';
+      }
+
+      
+
+      }
+    });
+  }
+
+  void reset(){
+    setState(() {
+      quote = 'Tap "New Quote" to get motivated ✨';
+      Symbol = '❓';
+    });
+  }
+
+  void random(){
+    random()
+  }
+.next.bool}
+
+@override 
+Widget build(BuildContext context){
+  return Scaffold(
+    body: Container(
+      color: Colors.blue,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$qoute',style:TextStyle(
+              fontStyle: 30,
+              backgroundColor: Colors.grey
+            ),)
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              elevatedbuttom(
+                onpressed=motivate
+              )
+            )
+
+          ],
+        )
+      )
+    ),
+  )
+
+}
+
